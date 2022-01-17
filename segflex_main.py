d944692d8bdc0f477a13cc1cfcae17684aad65d2
Master <ivanakhmetev@gmail.com>
2022-01-17 13:03:13 +0300

    adjust main to call project object

--- segflex_main.py
+++ segflex_main.py
@@ -1,17 +1,34 @@
 from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import pyqtSignal, QObject
 from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QGroupBox, QMainWindow, QFrame, QGridLayout,
                             QPushButton, QHBoxLayout, QTabWidget, QWidget, QLabel, QDialog,
                             QPlainTextEdit, QLineEdit, QMenu,
-                            QScrollArea, QToolButton, QSizePolicy)
+                            QScrollArea, QToolButton, QSizePolicy, QComboBox)
 
 import segflex_new_project
+import segflex_project_as_object as project
+
+class AnyObjects(QObject):
+    # создаем свой сигнал
+    own_signal = pyqtSignal()
+
 
 class main_window(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent, flags=QtCore.Qt.Window)
         
         self.adjust_main_window()
+        self.set_params()
         self.create_place_widgets_main_window()
+
+    def set_params(self):
+        self.ao = AnyObjects()
+        # обработчик сигнала, связанного с объектом
+        self.ao.own_signal.connect(self.on_clicked)
+        # параметры главного окна
+
+    def on_clicked(self):
+        print('Тут сообщение')
 
     def create_button_group(self):
         button_group = QGroupBox()
@@ -53,10 +70,87 @@ class main_window(QMainWindow):
         button_group_3.setLayout(layout)
         self.main_layout.addWidget(button_group_3, 0, 1)
 
-    def create_table(self): #QTabBar - более общий
+    def create_table(self):
+        table = QTabWidget()
+        
+        a1 = project.project_as_object(self)
+        a2 = project.project_as_object(self)
+        a3 = project.project_as_object(self)
+        a4 = project.project_as_object(self)
+        a5 = project.project_as_object(self)
+
+
+
+        self.scrollarea = QScrollArea(self)
+        #self.scrollarea.setFixedWidth(250)
+        self.scrollarea.setWidgetResizable(True)
+
+        widget = QWidget(self.scrollarea)
+        self.scrollarea.setWidget(widget)
+        self.layout_SArea = QVBoxLayout(widget)
+
+        self.layout_SArea.addWidget(a1)
+        self.layout_SArea.addWidget(a2)
+        self.layout_SArea.addWidget(a3)
+        self.layout_SArea.addWidget(a4)
+        self.layout_SArea.addWidget(a5)
+        widget.setMinimumSize(600, 400)
+        #widget.adjustSize()
+
+        tab2 = QWidget()
+        tab3 = QWidget()
+
+        table.addTab(self.scrollarea, "Проект")
+        table.addTab(tab2, "Редактирование")
+        table.addTab(tab3, "Просмотр")
+
+        #self.main_layout.addWidget(self.scrollarea, 1, 0, 3, 1)
+        self.main_layout.addWidget(table, 1, 0, 3, 1)
+
+    def create_table1(self): #QTabBar - более общий
         table = QTabWidget()
 
-        tab1 = QWidget()
+
+        #tab1 = QWidget()
+        tab1 = QGroupBox()
+
+        layout1 = QVBoxLayout()
+
+
+        a1 = project.project_as_object(self)
+        a2 = project.project_as_object(self)
+        a3 = project.project_as_object(self)
+        a4 = project.project_as_object(self)
+        a5 = project.project_as_object(self)
+        a6 = project.project_as_object(self)
+        
+        #tab1.setLayout(layout1)
+
+        #b1 = QPushButton("asd")
+        #b2 = QLabel("sd")
+        #layout1.addWidget(b1)
+        #layout1.addWidget(b2)
+
+        #layout1.addWidget(a1)
+        #layout1.addWidget(a2)
+        #layout1.addWidget(a3)
+        #layout1.addWidget(a4)
+        #layout1.addWidget(a5)
+        #layout1.addWidget(a6)
+
+
+        scroll = QScrollArea(self)
+        scroll.setWidgetResizable(True)
+        scroll.setWidget(tab1)
+        layout_small = QVBoxLayout(tab1)
+
+        layout_small.addWidget(a1)
+        layout_small.addWidget(a2)
+        layout_small.addWidget(a3)
+        layout_small.addWidget(a4)
+        layout_small.addWidget(a5)
+        layout_small.addWidget(a6)
+
         tab2 = QWidget()
         tab3 = QWidget()
 
@@ -88,6 +182,7 @@ class main_window(QMainWindow):
         main_frame = QFrame()
         self.setCentralWidget(main_frame)
         self.setWindowTitle("Segmentation app. 0.3")
+        self.resize(1000, 400)
 
         self.main_layout = QGridLayout()
         main_frame.setLayout(self.main_layout)
