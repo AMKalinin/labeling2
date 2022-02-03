29d8eb9c69150ac4ec787c5ecd394f5dfea2de72
Master <ivanakhmetev@gmail.com>
2022-02-03 11:37:22 +0300

    subclass qlabel for correct coords tracking

--- segflex_seg_window.py
+++ segflex_seg_window.py
@@ -1,18 +1,51 @@
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QGroupBox, QMainWindow, QFrame, QGridLayout,
                             QPushButton, QHBoxLayout, QTabWidget, QWidget, QLabel, QDialog,
                             QPlainTextEdit, QLineEdit, QMenu,
                             QScrollArea, QToolButton, QSizePolicy, QComboBox, QToolBar, 
                             QStatusBar)
 
-from PyQt5.QtGui import QImage, QPixmap, QIcon
+from PyQt5.QtGui import QImage, QPixmap, QIcon, QPainter, QColor, QFont, QBrush, QPen
 
 import os
 import h5py
 import numpy as np
 import cv2
 import segflex_classifier as classifier
+
+
+class myLabel(QLabel):
+    def __init__(self, parent=None):
+        QLabel.__init__(self, parent)
+
+
+    def mousePressEvent(display, event):
+        if event.button() == Qt.LeftButton:
+            print(event.pos())
+            #self.drawing = True
+            #self.lastPoint = event.pos()
+
+    """
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.LeftButton) & self.drawing:
+            painter = QPainter(self.canvas)
+            painter.setPen(QPen(self.brushColor, self.brushSize,
+                            Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            painter.drawLine(self.lastPoint, event.pos())
+            self.lastPoint = event.pos()
+
+            self.display.setPixmap(self.canvas)
+            #self.update()
+
+    def mouseReleaseEvent(self, event):
+ 
+        if event.button() == Qt.LeftButton:
+            # make drawing flag false
+            self.drawing = False
+            #self.display.setPixmap(self.canvas)
+
+    """
 
 class seg_window(QDialog):
     def __init__(self, parent=None, path=None):
@@ -25,6 +58,81 @@ class seg_window(QDialog):
         self.open_images_dir()
         self.open_image(self.identifier)
         self.create_navigation_bar()
+        self.create_instruments_bar()
+        self.draw_pencil_instruments()
+        
+    
+    def create_instruments_bar(self):
+        instruments_bar = QToolBar()
+
+        dots_instruments_btn = QToolButton()
+        dots_instruments_icon = QIcon()
+        dots_instruments_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_DOTS_INSTRUMENT_FULL))
+        dots_instruments_btn.setIcon(dots_instruments_icon)
+
+        instruments_bar.addWidget(dots_instruments_btn)
+
+        #dots_instruments_btn.clicked.connect(self.draw_dots_instruments)
+
+        self.layout.addWidget(instruments_bar, 1, 0)
+
+    def draw_pencil_instruments(self):
+        self.canvas = QPixmap(self.display.pixmap())
+        self.drawing = False
+        self.brushSize = 2
+        self.brushColor = Qt.black
+        self.lastPoint = QPoint()
+
+    """
+    def mousePressEvent(display, event):
+        if event.button() == Qt.LeftButton:
+            self.drawing = True
+            self.lastPoint = event.pos()
+
+    
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & Qt.LeftButton) & self.drawing:
+            painter = QPainter(self.canvas)
+            painter.setPen(QPen(self.brushColor, self.brushSize,
+                            Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
+            painter.drawLine(self.lastPoint, event.pos())
+            self.lastPoint = event.pos()
+
+            self.display.setPixmap(self.canvas)
+            #self.update()
+
+    def mouseReleaseEvent(self, event):
+ 
+        if event.button() == Qt.LeftButton:
+            # make drawing flag false
+            self.drawing = False
+            #self.display.setPixmap(self.canvas)
+    """
+    """
+    def paintEvent(self, event):
+        # create a canvas
+        canvasPainter = QPainter(self)
+         
+        # draw rectangle  on the canvas
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+    """
+
+
+
+    def drawing(self):
+        pixmap = QPixmap(self.display.pixmap())
+
+        self.drawing = False
+        self.brushSize = 2
+        self.brushColor = Qt.black
+        #pixmap.fill(Qt.transparent)
+        qp = QPainter(pixmap)
+        pen = QPen(Qt.red, 3)
+        qp.setPen(pen)
+        qp.drawLine(10, 10, 50, 50)
+        qp.end()
+        self.display.setPixmap(pixmap)
+
         #self.create_image_area()
         #self.initPre()
     
@@ -33,33 +141,34 @@ class seg_window(QDialog):
 
         previous_btn = QToolButton()
         previous_icon = QIcon()
-        previous_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_PREVIOUS_FULL), QIcon.Normal, QIcon.Off)
+        previous_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_PREVIOUS_FULL))
         previous_btn.setIcon(previous_icon)
 
         to_first_btn = QToolButton()
         to_first_icon = QIcon()
-        to_first_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_TOFIRST_FULL), QIcon.Normal, QIcon.Off)
+        to_first_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_TOFIRST_FULL))
         to_first_btn.setIcon(to_first_icon)
 
         to_last_btn = QToolButton()
         to_last_icon = QIcon()
-        to_last_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_TOLAST_FULL), QIcon.Normal, QIcon.Off)
+        to_last_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_TOLAST_FULL))
         to_last_btn.setIcon(to_last_icon)
 
         next_btn = QToolButton()
         next_icon = QIcon()
-        next_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_NEXT_FULL), QIcon.Normal, QIcon.Off)
+        next_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_NEXT_FULL))
         next_btn.setIcon(next_icon)
 
+        """
+        dots_instruments_btn = QToolButton()
+        dots_instruments_icon = QIcon()
+        dots_instruments_icon.addPixmap(QPixmap(classifier.ICON_SEG_TBTN_DOTS_INSTRUMENT_FULL))
+        dots_instruments_btn.setIcon(dots_instruments_icon)
+
+        navigation_bar.addWidget(dots_instruments_btn)
+        """
         self.image_position_postfix = ' / ' + str(self.image_position_max) 
         self.image_position_widget = QLabel(str(self.current_image_position) + self.image_position_postfix) 
-        #next_btn.setText("Next")
-
-
-        #toolButton.setCheckable(True)
-        #toolButton.setAutoExclusive(True)
-        #toolButton.setCheckable(True)
-        #toolButton.setAutoExclusive(True)
 
         navigation_bar.addWidget(to_first_btn)
         navigation_bar.addWidget(previous_btn)
@@ -72,20 +181,22 @@ class seg_window(QDialog):
         next_btn.clicked.connect(self.on_next)
         to_last_btn.clicked.connect(self.on_to_last)
 
-        self.layout.addWidget(navigation_bar, 0, 0, Qt.AlignTop | Qt.AlignHCenter) #области  
+        self.layout.addWidget(navigation_bar, 0, 0, 1, 2)#, Qt.AlignTop)# | Qt.AlignHCenter) #области  
+
         
     def on_to_first(self):
-        self.identifier = 0
-        self.current_image_position = 1
-        self.open_image(self.identifier)
-        self.update_image_position_widget()
+        if self.identifier != 0:
+            self.identifier = 0
+            self.current_image_position = 1
+            self.open_image(self.identifier)
+            self.update_image_position_widget()
 
     def on_to_last(self):
-        self.identifier = self.identifier_max
-        self.current_image_position = self.image_position_max
-        self.open_image(self.identifier)
-        self.update_image_position_widget()
-
+        if self.identifier != self.identifier_max:
+            self.identifier = self.identifier_max
+            self.current_image_position = self.image_position_max
+            self.open_image(self.identifier)
+            self.update_image_position_widget()
 
     def on_previous(self):
         if self.identifier > 0:
@@ -93,7 +204,6 @@ class seg_window(QDialog):
             self.current_image_position -= 1
             self.open_image(self.identifier)
             self.update_image_position_widget()
-
 
     def on_next(self):
         if self.identifier < self.identifier_max:
@@ -117,8 +227,8 @@ class seg_window(QDialog):
 
     def open_image(self, identifier):  #каждый раз открываю проблема закрыть корректно файл, если один раз
         self.clear_window_layout(self.image_layout)
-        self.display = QLabel()
-        self.display.setMaximumSize(600, 600)
+        self.display = myLabel()
+        self.display.setFixedSize(600,600)
         with h5py.File(self.path, 'r') as hdf:
             self.identifier_max = len(list(hdf[classifier.HDF_GROUP_SRCS_NAME].keys())) - 1 #starting with 0
             self.image_position_max = self.identifier_max + 1 #starting with 1
@@ -169,12 +279,12 @@ class seg_window(QDialog):
 
     def adjust_window(self):
         self.setWindowTitle("Разметка проекта")
-        self.setMinimumSize(800,800)
+        self.setFixedSize(800,800)
         #self.layout = QHBoxLayout()
         self.layout = QGridLayout()
         self.image_layout = QHBoxLayout()
         self.setLayout(self.layout)
-        self.layout.addLayout(self.image_layout, 1, 1, 1, 1) # правильно растянуть область изображения
+        self.layout.addLayout(self.image_layout, 2, 1) # правильно растянуть область изображения
         
     def initPre(self):
         """
