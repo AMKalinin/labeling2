f34c7a77097a08135531fdaf7e02637eebc48c09
Master <ivanakhmetev@gmail.com>
2022-02-14 16:39:20 +0300

    заполнение полигона ультра криво

--- segflex_draw_window.py
+++ segflex_draw_window.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QPointF
 from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QGroupBox, QMainWindow, QFrame, QGridLayout,
                             QPushButton, QHBoxLayout, QTabWidget, QWidget, QLabel, QDialog,
                             QPlainTextEdit, QLineEdit, QMenu,
                             QScrollArea, QToolButton, QSizePolicy, QComboBox, QToolBar, 
                             QStatusBar)
 
-from PyQt5.QtGui import QImage, QPixmap, QIcon, QPainter, QColor, QFont, QBrush, QPen
+from PyQt5.QtGui import QImage, QPixmap, QIcon, QPainter, QColor, QFont, QBrush, QPen, QPolygonF, QPainterPath, QRegion
 
 import os
 import h5py
@@ -18,6 +18,7 @@ import copy
 class Canvas(QLabel):
     def __init__(self, parent=None, pixmap=None, mask=None):
         super().__init__()
+        self.control_pencil = False
         self.pixmap = pixmap.copy()
         self.setPixmap(self.pixmap)
         self.set_pencil_instruments()
@@ -26,11 +27,13 @@ class Canvas(QLabel):
         self.init_mask()
     
     def init_mask(self):
-        self.mask = []
+        self.mask = QPolygonF()
+        self.mask_points = []
         #print("deep pixmap = ",id(self.pixmap), id(pixmap)) 
     
     def update_mask(self, point):
-        self.mask.append(point)
+        self.mask_points.append(QPointF(point))
+        self.mask.append(QPointF(point))
 
     def set_pencil_instruments(self):
         self.drawing = False
@@ -45,7 +48,7 @@ class Canvas(QLabel):
             self.update_mask(self.lastPoint)
 
     def mouseMoveEvent(self, event):
-        if (event.buttons() & Qt.LeftButton) & self.drawing:
+        if (event.buttons() & Qt.LeftButton) & self.drawing & self.control_pencil:
             painter = QPainter(self.pixmap)
             painter.setPen(QPen(self.brushColor, self.brushSize,
                             Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
@@ -58,6 +61,19 @@ class Canvas(QLabel):
         if event.button() == Qt.LeftButton:
             self.drawing = False
             self.setPixmap(self.pixmap)
+
+    def draw_polygon(self):
+        painter = QPainter(self.pixmap)
+        painter.drawPolygon(self.mask)
+
+    def fill_polygon(self):
+        painter = QPainter(self.pixmap)
+        #region = QRegion(self.mask)
+        self.painter_path = QPainterPath(self.mask_points[0])
+        self.painter_path.addPolygon(self.mask)
+        painter.fillPath(self.painter_path, Qt.black)
+
+
 
     """
     def paintEvent(self, event):
@@ -81,7 +97,6 @@ class drawing_dialog(QDialog):
 
         self.project_path = project_path
         self.identifier = identifier
-        self.mask = []
         self.adjust_window(window_geometry)
         self.create_place_connect_classes_buttons()
         self.create_canvas(canvas_pixmap, canvas_geometry)
@@ -90,19 +105,75 @@ class drawing_dialog(QDialog):
         self.create_control_btns()
 
     def create_place_connect_classes_buttons(self):
+
+        self.new_object_button = QPushButton("Новый объект класса")
+        self.new_object_button.setCheckable(False)
+        self.new_object_button.setChecked(False)
+        self.layout.addWidget(self.new_object_button, 1, 1)
+
+        self.control_pencil_button = QPushButton("Включить/выключить рисование")
+        self.control_pencil_button.setCheckable(True)
+        self.control_pencil_button.setChecked(False)
+        self.control_pencil_button.toggled["bool"].connect(self.on_control_pencil_button)
+        self.layout.addWidget(self.control_pencil_button, 1, 2)
+
+        self.draw_polygon_button = QPushButton("Нарисовать полигон")
+        self.draw_polygon_button.clicked.connect(self.on_draw_polygon_button)
+        self.layout.addWidget(self.draw_polygon_button, 1, 3)
+
+        self.fill_polygon_button = QPushButton("Заполнить полигон")
+        self.fill_polygon_button.clicked.connect(self.on_fill_polygon_button)
+        self.layout.addWidget(self.fill_polygon_button, 1, 4)
+
         with h5py.File(self.project_path, 'r') as hdf:
             self.classes_list = hdf.attrs[classifier.HDF_FILE_ATTR_CLASSES]
             print("classes in project = ", self.classes_list)
             i = 1 
             for el in self.classes_list:
                 button = QPushButton(el)
+                button.setCheckable(True)
+                button.setChecked(False)
+                button.toggled["bool"].connect(self.on_toggled_classes_buttons)
                 self.layout.addWidget(button, i, 0)
                 i += 1
 
+    def on_draw_polygon_button(self):
+        if self.canvas.mask:
+            self.canvas.draw_polygon()
+    
+    def on_fill_polygon_button(self):
+        #self.painter_path = QPainterPath(self.)
+        self.canvas.fill_polygon()
+        
+
+
+    
+    def on_control_pencil_button(self, status):
+        #print(status)
+        if status == True:
+            self.canvas.control_pencil = True
+        else:
+            self.canvas.control_pencil = False
+
+
+
+    def on_toggled_classes_buttons(self, status):
+        if status == True:
+            self.new_object_button.setCheckable(True)
+            self.new_object_button.setChecked(False)
+        else:
+            self.new_object_button.setCheckable(False)
+            self.new_object_button.setChecked(False)
+
+        #print("btn pressed", status)
 
     def create_group_for_objects_on_image(self):
         with h5py.File(self.project_path, 'r+') as hdf:
             parent_group = hdf[classifier.HDF_GROUP_OBJECT_LAYERS_NAME]
+            try:
+                parent_group[str(self.identifier)]
+            except BaseException:
+                parent_group.create_group(str(self.identifier))
             print(parent_group)
 
     def create_control_btns(self):
@@ -169,5 +240,5 @@ class drawing_dialog(QDialog):
         #self.canvas.setPixmap(canvas_pixmap)
         #self.canvas.setGeometry(canvas_geometry)
         #self.image_layout.addWidget(self.canvas)
-        self.layout.addWidget(self.canvas, 1,1)
+        self.layout.addWidget(self.canvas, 2,2)
     
