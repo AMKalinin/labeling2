afb3b46fb7e2cc0841dab6b0ed0fb4f10d1b3915
Master <ivanakhmetev@gmail.com>
2022-03-04 09:17:09 +0300

    сигнал по кнопке открыть задачи, вкладка задачи тест

--- segflex_main.py
+++ segflex_main.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QGroupBox, QMainWindow, 
 
 import segflex_new_project
 import segflex_project_as_widget as project
+import segflex_task_as_widget as task
 import os
 import json
 import segflex_classifier as classifier
@@ -38,6 +39,7 @@ class main_window(QMainWindow):
                     to_project_name = hdf.attrs[classifier.HDF_FILE_ATTR_NAME]
                     to_project_classes = hdf.attrs[classifier.HDF_FILE_ATTR_CLASSES]
                     project_widget = project.project_as_widget(name=to_project_name, classes=to_project_classes, path=project_full_name)
+                    project_widget.signal_parse_tasks.connect(self.parse_tasks)
                     self.layout_SArea.addWidget(project_widget)
 
 
@@ -88,7 +90,7 @@ class main_window(QMainWindow):
         self.main_layout.addWidget(button_group_3, 0, 1)
 
     def create_table(self):
-        table = QTabWidget()
+        self.table = QTabWidget()
         
         #a1 = project.project_as_widget(name="create_table", classes=[1,2,3])
         #a2 = project.project_as_widget(name="create_table", classes=[1,2,3])
@@ -116,16 +118,22 @@ class main_window(QMainWindow):
         #self.layout_SArea.addWidget(a5)
         #widget.setMinimumSize(600, 400)# //skukojivaet widgeti 
         #widget.adjustSize()
+        self.scrollarea2 = QScrollArea(self)
+        self.scrollarea2.setWidgetResizable(True)
+        widget2 = QWidget(self.scrollarea2)
+        self.scrollarea2.setWidget(widget2)
+        self.layout_tasks_box = QVBoxLayout(widget2)
+        #self.parse_tasks()
 
         tab2 = QWidget()
         tab3 = QWidget()
 
-        table.addTab(self.scrollarea, "Проект")
-        table.addTab(tab2, "Редактирование")
-        table.addTab(tab3, "Просмотр")
+        self.table.addTab(self.scrollarea, "Проекты")
+        self.table.addTab(self.scrollarea2, "Задачи")
+        self.table.addTab(tab3, "Просмотр")
 
         #self.main_layout.addWidget(self.scrollarea, 1, 0, 3, 1)
-        self.main_layout.addWidget(table, 1, 0, 3, 1)
+        self.main_layout.addWidget(self.table, 1, 0, 3, 1)
 
     def create_description(self, file_description=""): #рамка? фон? форматирование описания?
         description = QLabel(file_description)
@@ -136,6 +144,54 @@ class main_window(QMainWindow):
         #self.dialog.signal1.connect(self.create_new_project_file())
         self.dialog.signal1.connect(self.parse_projects_folder)
         self.dialog.exec_()
+
+    def parse_tasks(self, project_path):
+        self.clear_table_layout(layout=self.layout_tasks_box)
+        with h5py.File(project_path, 'r') as hdf: #ATTRS???
+            group_srcs = hdf[classifier.HDF_GROUP_SRCS_NAME]
+            number_of_images = len(group_srcs.keys())
+            for number in range(number_of_images):
+                task_widget = task.task_as_widget(classes=[], path=project_path, identifier=number)
+                self.layout_tasks_box.addWidget(task_widget)
+            self.table.setCurrentWidget(self.scrollarea2)
+            print(number_of_images, " __ ", project_path)
+
+
+    """
+    def open_image(self, identifier): 
+        self.clear_window_layout(self.image_layout)
+        self.display = seg_label.Label()
+        self.display.setFixedSize(600,600)
+        with h5py.File(self.project_path, 'r') as hdf:
+            self.identifier_max = len(list(hdf[classifier.HDF_GROUP_SRCS_NAME].keys())) - 1 #starting with 0
+            self.image_position_max = self.identifier_max + 1 #starting with 1
+            print(list(hdf[classifier.HDF_GROUP_SRCS_NAME].keys()))
+            print(self.identifier_max)
+            group_srcs = hdf[classifier.HDF_GROUP_SRCS_NAME]                
+            dataset = group_srcs[str(identifier)]
+            image_as_numpy = dataset[()]
+            height, width, channel = image_as_numpy.shape
+            bytesPerLine = 3 * width
+            image_as_qimage = QImage(image_as_numpy, width, height, bytesPerLine, QImage.Format_RGB888)
+            image_correct_rgb = image_as_qimage.rgbSwapped()
+            image_as_pixmap = QPixmap(image_correct_rgb)
+            self.display.update_base(image_as_pixmap)
+            self.image_layout.addWidget(self.display)
+    """
+    """
+    def parse_projects_folder(self):
+        self.clear_table_layout(layout=self.layout_SArea)
+        self.check_create_projects_folder()
+        projects_list = os.listdir(classifier.PROJECTS_FOLDER_FULL_NAME)
+        for project_short_name in projects_list:
+            if project_short_name.find(classifier.HDF_POSTFIX) != -1:
+                project_full_name = classifier.PROJECTS_FOLDER_FULL_NAME + '/' + project_short_name
+                with h5py.File(project_full_name, 'r') as hdf: #ATTRS???
+                    to_project_name = hdf.attrs[classifier.HDF_FILE_ATTR_NAME]
+                    to_project_classes = hdf.attrs[classifier.HDF_FILE_ATTR_CLASSES]
+                    project_widget = project.project_as_widget(name=to_project_name, classes=to_project_classes, path=project_full_name)
+                    self.layout_SArea.addWidget(project_widget)
+    """
 
     def create_place_widgets_main_window(self):
         self.create_button_group()
@@ -148,7 +204,7 @@ class main_window(QMainWindow):
     def adjust_main_window(self):
         main_frame = QFrame()
         self.setCentralWidget(main_frame)
-        self.setWindowTitle("Segmentation app. 0.7")
+        self.setWindowTitle("Segmentation app. 0.8")
         self.resize(1000, 400)
 
         self.main_layout = QGridLayout()
--- segflex_task_as_widget.py
+++ segflex_task_as_widget.py
@@ -8,28 +8,24 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import segflex_seg_window as seg_window
+import segflex_classifier as classifier
+import h5py
 
-"""
-class project_as_widget(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-"""
 
-class project_as_widget(QGroupBox):
-    Signal_OneParameter = pyqtSignal(str)
-    signal_parse_tasks = pyqtSignal(str)
+class task_as_widget(QGroupBox):
+    #Signal_OneParameter = pyqtSignal(str)
 
     def __init__(self,
-    name,
     classes,
     path,
+    identifier,
     parent=None,
     ide=0):
         #QGroupBox.__init__(self, name, classes, parent, ide)
         super().__init__()
-        print("name = " , name, "path = ", path)
-        self.path = path
-        self.id = ide
+        self.project_path = path
+        self.identifier = identifier
+
         layout = QHBoxLayout()
         layout_preview = QVBoxLayout()
         layout_info = QVBoxLayout()
@@ -45,11 +41,11 @@ class project_as_widget(QGroupBox):
 
 
         image = QLabel(self)
-        pixmap = QtGui.QPixmap("image.jpg")
+        pixmap = self.create_previw()
         image.setPixmap(pixmap)
-        image.setFixedSize(100, 100)
+        #image.setFixedSize(100, 100)
 
-        info_number = QLabel("#" + str(ide) + name)
+        info_number = QLabel("#" + str(ide) )#+ name)
         info_created_by = QLabel("Created by Hashly on November 1st 2021")
         info_last_update = QLabel("Last updated 15 days ago")
 
@@ -57,8 +53,8 @@ class project_as_widget(QGroupBox):
 
         jobs = QLabel("0 of 1 jobs")
 
-        self.btn_open = QPushButton("Открыть задачи")
-        self.btn_open.clicked.connect(self.on_open)
+        self.btn_open = QPushButton("Open")
+
         self.btn_delete = QPushButton("Delete")
         self.btn_delete.clicked.connect(self.emit_delete_signal)
         self.btn_edit = QPushButton("Edit")
@@ -87,25 +83,28 @@ class project_as_widget(QGroupBox):
         self.setMaximumHeight(120)
         self.setLayout(layout)
 
-    def on_open(self):
-        self.signal_parse_tasks.emit(self.path)
-        #pass
+    def create_previw(self): 
+        with h5py.File(self.project_path, 'r') as hdf:
+            group_srcs = hdf[classifier.HDF_GROUP_SRCS_NAME]                
+            dataset = group_srcs[str(self.identifier)]
+            image_as_numpy = dataset[()]
+            height, width, channel = image_as_numpy.shape
+            bytesPerLine = 3 * width
+            image_as_qimage = QImage(image_as_numpy, width, height, bytesPerLine, QImage.Format_RGB888)
+            image_correct_rgb = image_as_qimage.rgbSwapped()
+            image_as_pixmap = QPixmap(image_correct_rgb)
+            image_resized = image_as_pixmap.scaled(100, 100)
+
+            return image_resized
+
 
     def emit_delete_signal(self):
         #self.Signal_OneParameter.emit("date_str")
-        self.deleteLater()
+        #self.deleteLater()
+        pass
 
     def on_edit(self):
-        self.seg_window = seg_window.seg_window(self, self.path)
-        self.seg_window.exec_()
-
-
-
-
-if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    w = project_as_widget()
-    w.show()
-    sys.exit(app.exec_())
+        pass
+        #self.seg_window = seg_window.seg_window(self, self.path)
+        #self.seg_window.exec_()
 
