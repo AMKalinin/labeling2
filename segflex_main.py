c63e58de92a2126995e1627022de9875efb1bea6
Master <ivanakhmetev@gmail.com>
2022-03-18 12:43:23 +0300

    rebuild main

--- segflex_main.py
+++ segflex_main.py
@@ -8,8 +8,6 @@ from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QGroupBox, QMainWindow, 
 
 import segflex_new_project
 import segflex_project_as_widget as project
-import segflex_task_as_widget_to_do as task_todo
-import segflex_task_as_widget_to_check as task_tocheck
 import segflex_task_as_widget as task_base
 import os
 import json
@@ -20,18 +18,103 @@ import time
 
 class main_window(QMainWindow):
     signal_reparse = pyqtSignal(str)
-
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent, flags=QtCore.Qt.Window)
-
-        #super(main_window, self).__init__( parent) #? toje rabotaet
-
         self.adjust_main_window()
-        #self.check_projects_folder()
         self.create_place_widgets_main_window()
         self.signal_reparse.connect(self.parse_tasks)
 
+        #self.init_ui()
+    
+    def init_ui(self):
+        self.check_create_projects_folder()
+        self.create_layouts()
+        self.fill_layouts()
+        self.place_layouts()
 
+        adjust_window()
+
+    def create_layouts(self):
+        self.main_layout = QGridLayout()
+        #self.tab_projects_layout = QVBoxLayout()
+        #self.tab_tasks_left_layout = QVBoxLayout()
+        #self.tab_tasks_right_layout = QVBoxLayout()
+
+    def fill_layouts(self):
+        self.init_widgets()
+        pass
+
+
+    def place_layouts(self):
+        self.main_frame.setLayout(self.main_layout)
+
+        #self.tab.addTab(self.tab_project_area, "Проекты")
+        #self.tab.addTab(self.tab_split, "Задачи")
+
+        self.main_layout.addWidget(self.table, 0, 0, 4, 1)
+        #self.tab.addTab(tab3, "Просмотр")
+        
+    def init_widgets(self):
+        self.init_table()
+        self.parse_table_projects()
+        self.init_somebtns()
+
+
+    def init_table(self):
+        self.tab = QTabWidget()
+        self.tab_projects_area = QScrollArea(self)
+        self.tab_tasks_left_area = QScrollArea(self)
+        self.tab_tasks_right_area = QScrollArea(self)
+
+        self.tab_projects_area.setWidgetResizable(True)
+        self.tab_tasks_left_area.setWidgetResizable(True)
+        self.tab_tasks_right_area.setWidgetResizable(True)
+
+        self.tab_split = QSplitter()
+        self.tab_split.addWidget(self.tab_tasks_left_area)
+        self.tab_split.addWidget(self.tab_tasks_right_area)
+
+        self.tab_projects_group = QGroupBox(self.tab_projects_area)
+        self.tab_tasks_left_group = QGroupBox(self.tab_tasks_left_area)
+        self.tab_tasks_right_group = QGroupBox(self.tab_tasks_right_area)
+
+        self.tab_projects_group.setTitle("Проекты")
+        self.tab_tasks_left_group.setTitle("Разметка")
+        self.tab_tasks_right_group.setTitle("Контроль и редактирование")
+
+        self.tab_projects_area.setWidget(self.tab_projects_group)
+        self.tab_tasks_left_area.setWidget(self.tab_tasks_left_group)
+        self.tab_tasks_right_area.setWidget(self.tab_tasks_right_group)
+
+        self.tab_projects_layout = QVBoxLayout()
+        self.tab_tasks_left_layout = QVBoxLayout()
+        self.tab_tasks_right_layout = QVBoxLayout()
+
+        self.tab_projects_group.setLayout(self.tab_projects_layout)
+        self.tab_tasks_left_group.setLayout(self.tab_tasks_left_layout)
+        self.tab_tasks_right_group.setLayout(self.tab_tasks_right_layout)
+
+        self.tab.addTab(self.tab_projects_area, "Проекты")
+        self.tab.addTab(self.tab_split, "Задачи")
+
+        self.parse_projects_folder()
+
+
+        self.tab.addTab(self.tab_projects_area, "Проекты")
+        self.tab.addTab(self.tab_split, "Задачи")
+
+        self.main_layout.addWidget(self.tab, 0, 0, 4, 1)
+
+
+    def adjust_main_window(self):
+        self.main_frame = QFrame()
+        self.setCentralWidget(self.main_frame)
+        self.main_layout = QGridLayout()
+        self.main_frame.setLayout(self.main_layout)
+        self.setWindowTitle("Segmentation app. 0.8")
+        self.resize(1000, 400)
+
+    """
     def parse_projects_folder(self):
         self.clear_table_layout(layout=self.layout_SArea)
         self.check_create_projects_folder()
@@ -47,6 +130,20 @@ class main_window(QMainWindow):
                     #project_widget.signal_parse_tasks.connect(self.parse_tasks)
                     #self.signal_reparse.connect(self.parse_tasks)
                     self.layout_SArea.addWidget(project_widget)
+
+    """
+
+    def parse_projects_folder(self):
+        self.clear_table_layout(layout=self.tab_projects_layout)
+        projects_list = os.listdir(classifier.PROJECTS_FOLDER_FULL_NAME)
+        for project_short_name in projects_list:
+            if project_short_name.find(classifier.HDF_POSTFIX) != -1:
+                project_full_name = classifier.PROJECTS_FOLDER_FULL_NAME + '/' + project_short_name
+                with h5py.File(project_full_name, 'r') as hdf:
+                    project_name = hdf.attrs[classifier.HDF_FILE_NAME]
+                    project_classes = hdf.attrs[classifier.HDF_FILE_CLASSES]
+                    project_widget = project.project_as_widget(name=project_name, classes=project_classes, path=project_full_name, signal= self.signal_reparse)
+                    self.tab_projects_layout.addWidget(project_widget)
 
 
     def test_create_widget(self):
@@ -98,21 +195,16 @@ class main_window(QMainWindow):
     def create_table(self):
         self.table = QTabWidget()
         
-        #a1 = project.project_as_widget(name="create_table", classes=[1,2,3])
-        #a2 = project.project_as_widget(name="create_table", classes=[1,2,3])
-        #a3 = project.project_as_widget(self)
-        #a4 = project.project_as_widget(self)
-        #a5 = project.project_as_widget(self)
-
-
-
         self.scrollarea = QScrollArea(self)
         #self.scrollarea.setFixedWidth(250)
         self.scrollarea.setWidgetResizable(True)
 
         widget = QWidget(self.scrollarea)
         self.scrollarea.setWidget(widget)
-        self.layout_SArea = QVBoxLayout(widget)
+        #self.layout_SArea = QVBoxLayout(widget)
+        self.layout_SArea = QVBoxLayout()
+        widget.setLayout(self.layout_SArea)
+        #self.layout_SArea.setParent(self.scrollarea)
 
 
         self.parse_projects_folder()
@@ -167,8 +259,8 @@ class main_window(QMainWindow):
         self.dialog.exec_()
 
     def parse_tasks(self, project_path):
-        self.clear_table_layout(layout=self.layout_tasks_box)
-        self.clear_table_layout(layout=self.layout_tasks_box2)
+        self.clear_table_layout(layout=self.tab_tasks_left_layout)
+        self.clear_table_layout(layout=self.tab_tasks_right_layout)
         with h5py.File(project_path, 'r') as hdf: #ATTRS???
             group_srcs = hdf[classifier.HDF_GROUP_SRCS_NAME]
             number_of_images = len(group_srcs.keys())
@@ -176,74 +268,23 @@ class main_window(QMainWindow):
                 status = group_srcs[str(number)].attrs[classifier.HDF_TASK_STATUS]
                 if status == classifier.HDF_TASK_STATUS_0 or status == classifier.HDF_TASK_STATUS_1:
                     task_widget = task_base.task_widget(path=project_path, identifier=number, mode=classifier.TASK_WIDGET_MODE_0, signal=self.signal_reparse)
-                    self.layout_tasks_box.addWidget(task_widget)
+                    self.tab_tasks_left_layout.addWidget(task_widget)
                 if status == classifier.HDF_TASK_STATUS_2 or status == classifier.HDF_TASK_STATUS_3:
                     task_widget = task_base.task_widget(path=project_path, identifier=number, mode=classifier.TASK_WIDGET_MODE_1, signal=self.signal_reparse)
-                    self.layout_tasks_box2.addWidget(task_widget)
+                    self.tab_tasks_right_layout.addWidget(task_widget)
                 #task_widget.signal_reparse.connect(self.callreparse(project_path))
-            self.table.setCurrentWidget(self.scrollarea2)
+            #self.table.setCurrentWidget(self.scrollarea2)
             
         print(number_of_images, " _2_ ", project_path)
 
-    def testPriunt(self):
-        
-        print("asd")
-
-    def callreparse(self, project_path):
-        self.parse_tasks(project_path)
-
-    """
-    def open_image(self, identifier): 
-        self.clear_window_layout(self.image_layout)
-        self.display = seg_label.Label()
-        self.display.setFixedSize(600,600)
-        with h5py.File(self.project_path, 'r') as hdf:
-            self.identifier_max = len(list(hdf[classifier.HDF_GROUP_SRCS_NAME].keys())) - 1 #starting with 0
-            self.image_position_max = self.identifier_max + 1 #starting with 1
-            print(list(hdf[classifier.HDF_GROUP_SRCS_NAME].keys()))
-            print(self.identifier_max)
-            group_srcs = hdf[classifier.HDF_GROUP_SRCS_NAME]                
-            dataset = group_srcs[str(identifier)]
-            image_as_numpy = dataset[()]
-            height, width, channel = image_as_numpy.shape
-            bytesPerLine = 3 * width
-            image_as_qimage = QImage(image_as_numpy, width, height, bytesPerLine, QImage.Format_RGB888)
-            image_correct_rgb = image_as_qimage.rgbSwapped()
-            image_as_pixmap = QPixmap(image_correct_rgb)
-            self.display.update_base(image_as_pixmap)
-            self.image_layout.addWidget(self.display)
-    """
-    """
-    def parse_projects_folder(self):
-        self.clear_table_layout(layout=self.layout_SArea)
-        self.check_create_projects_folder()
-        projects_list = os.listdir(classifier.PROJECTS_FOLDER_FULL_NAME)
-        for project_short_name in projects_list:
-            if project_short_name.find(classifier.HDF_POSTFIX) != -1:
-                project_full_name = classifier.PROJECTS_FOLDER_FULL_NAME + '/' + project_short_name
-                with h5py.File(project_full_name, 'r') as hdf: #ATTRS???
-                    to_project_name = hdf.attrs[classifier.HDF_FILE_NAME]
-                    to_project_classes = hdf.attrs[classifier.HDF_FILE_ATTR_CLASSES]
-                    project_widget = project.project_as_widget(name=to_project_name, classes=to_project_classes, path=project_full_name)
-                    self.layout_SArea.addWidget(project_widget)
-    """
 
     def create_place_widgets_main_window(self):
         self.create_button_group()
         #self.create_button_group_2()
         self.create_button_group_3()
-        self.create_table()
+        #self.create_table()
+        self.init_table()
         #self.create_description("Here must be project description")
-
-
-    def adjust_main_window(self):
-        main_frame = QFrame()
-        self.setCentralWidget(main_frame)
-        self.setWindowTitle("Segmentation app. 0.8")
-        self.resize(1000, 400)
-
-        self.main_layout = QGridLayout()
-        main_frame.setLayout(self.main_layout)
 
 
     def clear_table_layout(self, layout):
